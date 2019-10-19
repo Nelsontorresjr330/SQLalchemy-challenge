dd217db92d47cf46abe43d87e593e93b0f097480
Nelson <nelsontorresjr330>
2019-10-19 00:26:00 -0500

    Finished app.py

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 #Import all my dependencies at once
-import matplotlib
-from matplotlib import style
-import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import datetime as dt
@@ -9,6 +6,7 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
 
 #Create an engine for the `hawaii.sqlite` database
 engine = create_engine("sqlite:///Homework 10/Resources/hawaii.sqlite")
@@ -27,8 +25,8 @@ session = Session(engine)
 #Create the app
 app = Flask(__name__)
 
-# Calculate the date 1 year ago from the last data point in the database
-newestdate = (session.query(Measurement.date).order_by(Measurement.date.desc()).first())
+#Calculate the date 1 year ago from the last data point in the database
+newestdate = (session.query(Measure.date).order_by(Measure.date.desc()).first())
 newestdate = newestdate[0]
 year = newestdate.split('-')[0]
 monf = newestdate.split('-')[1]
@@ -36,88 +34,56 @@ day = newestdate.split('-')[2]
 yearold = int(year) - 1
 yearago = dt.datetime(yearold, int(monf), int(day))
 
-@app.route("/")
-def home():
+@app.route("/index")
+def index():
+    #Displays a homepage menu
     return (f"SQLAlchemy Homework - Climate App<br/><br/>"
             f"Available Routes:<br/>"
             f"/api/v1.0/stations => List of stations<br/>"
-            f"/api/v1.0/precipitaton => Precipitation data from the last year<br/>"
+            f"/api/v1.0/precipitation => Precipitation data from the last year<br/>"
             f"/api/v1.0/temperature => Temperature data from the last year<br/><br/>"
             f"/api/v1.0/datesearch/XXXX-XX-XX => Temperature data for date given and newer<br/>"
-            f"/api/v1.0/datesearch/XXXX-XX-XX/2016-01-30 => Temperature data for dates given and inbetween <br/>"
+            f"/api/v1.0/datesearch/XXXX-XX-XX/XXXX-XX-XX => Temperature data for dates given and inbetween <br/>"
             )
 
 @app.route("/api/v1.0/stations")
 def stations():
-    stations = session.query(Station.name).all()
-    #all_stations = list(np.ravel(results))
-    return jsonify(stations)
+    #Queries and prints all the station names
+    session = Session(engine)
+    stationquery = session.query(Station.name).all()
+    return jsonify(stationquery)
 
-@app.route("/api/v1.0/precipitaton")
+@app.route("/api/v1.0/precipitation")
 def precipitation():
-    percip = (session.query(Measurement.date, Measurement.prcp).filter(Measurement.date > yearBefore)
-            .order_by(Measurement.date).all())
-    
-    # preciparray = []
-    # for row in percip:
-    #     precipDict = {result.date: result.prcp, "Station": result.station}
-    #     precipData.append(precipDict)
-
+    #Queries and prints the percipation from the last year
+    session = Session(engine)
+    percip = (session.query(Measure.date, Measure.prcp).filter(Measure.date >= yearago)
+            .order_by(Measure.date).all())
     return jsonify(percip)
 
-@app.route("/api/v1.0/temperature")
+@app.route("/api/v1.0/tobs")
 def temperature():
+    #Queries and prints the temperature from the last year
+    session = Session(engine)
+    tempquery = (session.query(Measure.date, Measure.tobs).filter(Measure.date >= yearago)
+                      .order_by(Measure.date).all())
+    return jsonify(tempquery)
 
-    results = (session.query(Measurement.date, Measurement.tobs, Measurement.station)
-                      .filter(Measurement.date > yearBefore)
-                      .order_by(Measurement.date)
-                      .all())
+@app.route('/api/v1.0/<start>')
+def start(start):
+    #Queries and prints min, avg, and max after a certain date
+    session = Session(engine)
+    datequery =  (session.query(Measure.date, func.min(Measure.tobs), func.avg(Measure.tobs), func.max(Measure.tobs))
+                .filter(Measure.date >= start).group_by(Measure.date).all())
+    return jsonify(datequery)
 
-    tempData = []
-    for result in results:
-        tempDict = {result.date: result.tobs, "Station": result.station}
-        tempData.append(tempDict)
-
-    return jsonify(tempData)
-
-@app.route('/api/v1.0/datesearch/<startDate>')
-def start(startDate):
-    sel = [Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
-
-    results =  (session.query(*sel)
-                       .filter(func.strftime("%Y-%m-%d", Measurement.date) >= startDate)
-                       .group_by(Measurement.date)
-                       .all())
-
-    dates = []                       
-    for result in results:
-        date_dict = {}
-        date_dict["Date"] = result[0]
-        date_dict["Low Temp"] = result[1]
-        date_dict["Avg Temp"] = result[2]
-        date_dict["High Temp"] = result[3]
-        dates.append(date_dict)
-    return jsonify(dates)
-
-@app.route('/api/v1.0/datesearch/<startDate>/<endDate>')
-def startEnd(startDate, endDate):
-    sel = [Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
-
-    results =  (session.query(*sel)
-                       .filter(func.strftime("%Y-%m-%d", Measurement.date) >= startDate)
-                       .filter(func.strftime("%Y-%m-%d", Measurement.date) <= endDate)
-                       .group_by(Measurement.date)
-                       .all())
-
-    dates = []                       
-    for result in results:
-        date_dict = {}
-        date_dict["Date"] = result[0]
-        date_dict["Low Temp"] = result[1]
-        date_dict["Avg Temp"] = result[2]
-        date_dict["High Temp"] = result[3]
-        dates.append(date_dict)
-    return jsonify(dates)
+@app.route('/api/v1.0/<start>/<end>')
+def startEnd(start, end):
+    #Queries and prints min, avg, and max in between two dates
+    session = Session(engine)
+    datequery =  (session.query(Measure.date, func.min(Measure.tobs), func.avg(Measure.tobs), func.max(Measure.tobs))
+                .filter(Measure.date >= start).filter(Measure.date <= end).group_by(Measure.date).all())
+    return jsonify(datequery)
 
 if __name__ == "__main__":
     app.run(debug=True)
